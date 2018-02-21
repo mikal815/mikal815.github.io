bd13ae7fe41ee5ee5b3918150ff3d322a3200e23
Michael Mensinger <mikal815@gmail.com>
2018-02-20 20:03:37 -0500

    added stuff

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 console.log('welcome');
+
+// set a cookie with their name in it
+Cookies.set('firstName', 'Mike');
+
+// on click of #button,
+document.getElementById('button').addEventListener('click', function(){
+  // get the .value of #fname
+  var fName = document.getElementById('fname').value;
+  console.log(fName);
+  // store that value in a cookie
+  Cookies.set('firstName', fName);
+});
