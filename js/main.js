597620fb36d55330bc2f3233f86248903eeb074e
Michael Mensinger <mikal815@gmail.com>
2018-02-20 20:22:33 -0500

    modified main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ console.log('welcome');
 // By default (on page load)
 
 // if a 'firstName' cookie is set
+if(Cookies.get('firstName')){
+  alert('found a cookie named firstName')
+} else {
+  alert('no cookie is found')
+}
 // update the DOM to display the value of the firstName cookie
 
 
@@ -13,6 +18,7 @@ console.log('welcome');
 document.getElementById('button').addEventListener('click', function(){
   // get the .value of #fname
   var fName = document.getElementById('fname').value;
+
   console.log(fName);
   // store that value in a cookie
   Cookies.set('firstName', fName);
