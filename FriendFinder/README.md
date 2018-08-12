75ac242266b7ec4fb308125ab7e117f330b46341
Mike M <mikal815@users.noreply.github.com>
2018-08-11 21:41:03 -0400

    Added FriendFinder app

---
+++ FriendFinder/README.md
@@ -0,0 +1,2 @@
+# FriendFinder
+This is an app which utilizes node and express to compare answers to survey questions and match you with others who answered similarly to their survey questions.
---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,101 @@
+var friendArray =[
+    {
+        "name": "Spicoli",
+        "photo": "https://www.cannabisculture.com/wp-content/uploads/2016/08/film-fast-times-at-ridgemont-high-1982-jeff-spicoli-sean-penn-tshirts-ZsnArN-quote.jpg",
+        "scores": [
+            5,
+            5,
+            3,
+            2,
+            1,
+            4,
+            5,
+            3,
+            1,
+            4
+        ]
+    },
+    {
+        "name": "Redman",
+        "photo": "http://archive.massappeal.com/wp-content/uploads/redman-cribs.jpg",
+        "scores": [
+            3,
+            2,
+            1,
+            4,
+            2,
+            2,
+            2,
+            3,
+            3,
+            4
+
+        ]
+    },
+    {
+        "name": "Deniro",
+        "photo": "http://hergunhdfilm.com/uploads/oyuncu/2018/01/robert-de-niro-477.jpg",
+        "scores": [
+            3,
+            4,
+            2,
+            1,
+            5,
+            5,
+            5,
+            3,
+            2,
+            1,
+        ]
+    },
+    {
+        "name": "Homer",
+        "photo": "https://www.rtor.org/wp-content/uploads/2016/05/homer.jpg",
+        "scores": [
+            5,
+            4,
+            2,
+            2,
+            5,
+            4,
+            3,
+            1,
+            1,
+            1,
+        ]
+    },
+    {
+        "name": "Slash",
+        "photo": "https://ksassets.timeincuk.net/wp/uploads/sites/55/2010/11/081121_150422_slash1_PA-2-261x320.jpg",
+        "scores": [
+            1,
+            1,
+            4,
+            4,
+            3,
+            4,
+            5,
+            1,
+            1,
+            1,
+        ]
+    },
+    {
+        "name": "Dave Chappelle",
+        "photo": "https://pixel.nymag.com/imgs/daily/vulture/2018/01/02/dave/02-dave-chappelle-7.w710.h473.jpg",
+        "scores": [
+            5,
+            5,
+            4,
+            4,
+            3,
+            4,
+            5,
+            1,
+            3,
+            1,
+        ]
+    }
+];
+
+module.exports = friendArray;
