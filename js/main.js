40c23a34b65db56ec53970e454d1c11ea68a0042
Mike M <mikal815@users.noreply.github.com>
2018-11-14 06:25:10 -0500

    POST

--- js/main.js
+++ js/main.js
@@ -338,7 +338,7 @@
                 $.ajax({
     
                     type: "POST",
-                    url: "./inc/sendEmail.php",
+                    url: "inc/sendEmail.php",
                     data: $(form).serialize(),
                     beforeSend: function() { 
     
