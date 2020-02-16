41a625669ff588820bb88528ee68dbf83321605f
Mike M <mikal815@users.noreply.github.com>
2020-02-15 23:20:24 -0500

    begin revamp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import Header from '../src/components/Header';
+import styled from 'styled-components';
+import cyberstreet from '../src/assets/images/cyberstreet.jpg'
+
+const AppLayout = styled.div`
+  display: grid;
+  justify-content: center;
+  height: 100vh;
+  background-image: url(${cyberstreet});
+`
+const Button = styled.button`
+  height: 30px;
+  width: 150px;
+  border: 0;
+  border-radius: 0.5em;
+  outline: none;
+  color: #fff;
+  background-color: ${(props) => props.primary && 'green'};
+  &:hover {
+    background-color: red;
+  }
+`
 
 function App() {
+
   return (
-    <div className="App">
-      App
-    </div>
+
+    <AppLayout backgroundImage>
+      <Header />
+      <Button primary>CLICK ME</Button>
+    </AppLayout>
+
   );
 }
 
