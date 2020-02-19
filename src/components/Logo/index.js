f9b9019ff25b187727a98c730615714fe231b853
Mike M <mikal815@users.noreply.github.com>
2020-02-18 22:58:14 -0500

    Add 100% width to AppLayout/drawer slide looks better

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import logo from '../../assets/images/MMwhitelogo.png'
-import '../Logo/Logo.css'
+import logo from '../../assets/images/MMwhitelogo.png';
+import '../Logo/Logo.css';
+
 
 export default function Logo() {
     return (
+
         <img className="logo" src={logo} alt="Logo" />
     );
 }
