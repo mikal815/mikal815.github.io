c8f2c8ed836ca2313fe48018bb3a91ad3156e5d8
Mike M <mikal815@users.noreply.github.com>
2020-03-16 23:12:42 -0400

    Import images into Carousel

--- src/components/SlideCarousel/index.js
+++ src/components/SlideCarousel/index.js
@@ -1,32 +1,59 @@
 import React, { Component } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
+import { makeStyles } from '@material-ui/core/styles';
+import SurfersDepot from '../../assets/images/surfersdepot.PNG';
+import ShapeShifter from '../../assets/images/Shapes.PNG';
+import MTME from '../../assets/images/mtme.PNG';
+import BPS from '../../assets/images/BPS.PNG';
+import Icarus from '../../assets/images/Icarus.PNG';
 
-export default class SlideCarousel extends Component {
+const useStyles = makeStyles({
+    root: {
+        minWidth: 260,
+        maxWidth: 260,
+        height: 260,
+        backgroundColor: 'rgb(34, 125, 91)',
+        color: '#ffffff'
+    }
+});
+
+
+
+class SlideCarousel extends Component {
+
+
     render() {
         return (
-            <Carousel>
+            <Carousel
+                maxWidth={'260px'}
+                minWidth={'260px'}
+            >
                 <div>
-                    <img alt="Surfer's Depot" src="../../assets/images/surfersdepot.PNG" />
+                    <img alt="Surfer's Depot" src={SurfersDepot} />
                     <p className="legend">Surfer's Depot</p>
                 </div>
                 <div>
-                    <img alt="Icarus" src="../../assets/images/Icarus.PNG" />
-                    <p className="legend">Icarus</p>
-                </div>
-                <div>
-                    <img alt="Shape Shifter" src="../../assets/images/Shapes.PNG" />
+                    <img alt="Shape Shifter" src={ShapeShifter} />
                     <p className="legend">Shape Shifter</p>
                 </div>
                 <div>
-                    <img alt="Boardwalk Pier Stage" src="../../assets/images/BPS.PNG" />
+                    <img alt="Music to My Ears" src={MTME} />
+                    <p className="legend">Music to My Ears</p>
+                </div>
+                <div>
+                    <img alt="Boardwalk Pier Stage" src={BPS} />
                     <p className="legend">Boardwalk Pier Stage</p>
                 </div>
                 <div>
-                    <img alt="Music to My Ears" src="../../assets/images/mtme.PNG" />
-                    <p className="legend">Music to My Ears</p>
+                    <img alt="Icarus" src={Icarus} />
+                    <p className="legend">Icarus</p>
                 </div>
-            </Carousel>
+
+            </Carousel >
         );
     }
 };
+
+export default SlideCarousel
+
