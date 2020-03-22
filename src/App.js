e1d4000b2dc1a1b962ccaa3f3028fd6cd175f495
Mike M <mikal815@users.noreply.github.com>
2020-03-22 17:19:25 -0400

    Format for mobile inner height

--- src/App.js
+++ src/App.js
@@ -8,12 +8,16 @@ import MediaQuery from 'react-responsive';
 import SlideCarousel from './components/SlideCarousel';
 import CardFlip from '../src/components/CardFlip';
 
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 const AppLayout = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   grid-template-rows: 2rem 17rem 2rem 17rem 1rem;
   grid-template-rows: 10% 28% 10% 40% 10%;
   height: 100vh;
+  height: calc(var(--vh, 1vh) * 100);
   background-color: #27292b;
 `
 
