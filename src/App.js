c6ea7d9a68ec1a3c76470d797f21e90d7a86afce
Mike M <mikal815@users.noreply.github.com>
2020-03-31 23:00:49 -0400

    Remove card flip

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 import '../src/App.css';
 import Logo from '../src/components/Logo';
 import Sidebar from './components/Menu';
+import SimpleCard from '../src/components/SimpleCard';
 import MediaQuery from 'react-responsive';
 import SlideCarousel from './components/SlideCarousel';
-import CardFlip from '../src/components/CardFlip';
 
 // let vh = window.innerHeight * 0.01;
 // document.documentElement.style.setProperty('--vh', `${vh}px`);
@@ -13,7 +13,7 @@ import CardFlip from '../src/components/CardFlip';
 const AppLayout = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: 2rem 17rem 2rem 17rem 1rem;
+  /* grid-template-rows: 2rem 17rem 2rem 17rem 1rem; */
   grid-template-rows: 10% 28% 10% 40% 10%;
   height: 100vh;
   /* height: calc(var(--vh, 1vh) * 100); */
@@ -23,31 +23,33 @@ const AppLayout = styled.div`
 function App() {
 
   return (
-    <AppLayout>
-      <Logo />
-      <Sidebar />
+    <div>
+      <AppLayout>
+        <Logo />
+        <Sidebar />
 
-      <div className="span-row-2">
-        <MediaQuery maxDeviceWidth={500}>
-          <CardFlip />
-        </MediaQuery>
-      </div>
+        <div className="span-row-2">
+          <MediaQuery maxDeviceWidth={500}>
+            <SimpleCard />
+          </MediaQuery>
+        </div>
 
-      <button className="view-work span-row-2">
-        <div>View Work</div>
-      </button>
+        <button className="view-work span-row-2">
+          <div>View Work</div>
+        </button>
 
-      <div className="span-row-2">
-        <SlideCarousel />
-      </div>
+        <div className="span-row-2">
+          <SlideCarousel />
+        </div>
 
-      <div className="footer span-row-2">
-        <div>portfolio</div>
-        <div>skills</div>
-        <div>hire me</div>
-      </div>
+        <div className="footer span-row-2">
+          <div>portfolio</div>
+          <div>skills</div>
+          <div>hire me</div>
+        </div>
 
-    </AppLayout>
+      </AppLayout>
+    </div>
   )
 }
 
