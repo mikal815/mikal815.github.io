7edc7b3da5ddccf47d266aa2626b3b69fb1f8e72
Mike M <mikal815@users.noreply.github.com>
2020-04-18 19:46:28 -0400

    Style cards

--- src/components/SkillsCard/index.js
+++ src/components/SkillsCard/index.js
@@ -4,20 +4,23 @@ import styled from 'styled-components';
 const SkillsCard = styled.div`
    display: grid;
    justify-items: center;
-   justify-content: center;
-   font-family: "Helvetica Neue","Helvetica",Helvetica,Arial,sans-serif;
    align-items: center;
-    border-radius: 3px;
-    width: 25vw;
-    min-height: 15vh;
-    max-height: 23vh;
-    min-width: 15vh;
-    max-width: 23vh;
-    margin-right: 1rem;
-    margin-left: 1rem;
-    background-color: #227d5b;
-    color: #ffffff;
-    padding: 10px;
+   color: #29272b;
+   font-size: 1rem;
+   font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+   font-weight: 400;
+   line-height: 1.5;
+   letter-spacing: 0.00938em;
+   border-radius: 3px;
+   width: 25vw;
+   min-height: 15vh;
+   max-height: 23vh;
+   min-width: 15vh;
+   max-width: 23vh;
+   margin-right: 1rem;
+   margin-left: 1rem;
+   background-color: #227d5b;
+   padding: 10px;
 `
 
 export default function (props) {
