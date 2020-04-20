31f47a5dff9f7bfabba33d31446f9c26f2471523
Mike M <mikal815@users.noreply.github.com>
2020-04-19 21:33:22 -0400

    Format page

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const MobileLayout = styled.div`
 const TabletLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
-  /* grid-auto-rows: 1fr; */
+  grid-auto-rows: 5rem 1fr 1fr 1fr 1fr 1fr;
   height: 100%;
   background-color: #27292b;
   position: fixed;
