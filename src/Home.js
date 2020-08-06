74f2c598009be16b0895828055bfd76e9d545bcd
Mike M <mikal815@users.noreply.github.com>
2020-08-05 21:59:58 -0400

    Format tablet view

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import '../src/Home.css';
 import Logo from './components/Logo';
 import MenuMobile from './components/MenuMobile';
+import MenuTablet from './components/MenuTablet';
 import Menu from './components/Menu';
 import MediaQuery from 'react-responsive';
 import SlideCarousel from './components/SlideCarousel';
@@ -27,6 +28,17 @@ const MobileLayout = styled.div`
 `
 
 const TabletLayout = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr 14em auto;
+  height: 100%;
+  background-color: #27292b;
+  position: fixed;
+  padding-left: 5vw;
+  padding-right: 5vw;
+`
+
+const LaptopLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-template-rows: 1fr 1fr 1fr 1fr 14em auto;
@@ -67,8 +79,77 @@ function Home() {
                 </MobileLayout>
             </MediaQuery>
 
-            <MediaQuery minDeviceWidth={500}>
+            <MediaQuery minDeviceWidth={500} maxDeviceWidth={850}>
                 <TabletLayout>
+                    <Logo />
+                    <MenuTablet />
+
+                    <div className="slogan-container span-row-2">
+                        <div className="slogan-text">
+                            I actualize intuative and<br></br>engaging experiences ~
+                        </div>
+                    </div>
+
+                    <div className="title span-row-2">
+                        Software Developer &nbsp; | &nbsp; Philadelphia, PA
+                    </div>
+
+                    <div className="skills-container span-row-2">
+                        <div className="skills-card skills-card-frontend">
+                            <img src={frontend} alt="frontend icon" />
+                            <div className="heading">FRONT END</div>
+                            <div>HTML5</div>
+                            <div>CSS3 | SASS</div>
+                            <div>Javascript</div>
+                            <div>React | Redux</div>
+                            <div className="hidden-dash">-</div>
+                            <div className="hidden-dash">-</div>
+                        </div>
+                        <div className="skills-card skills-card-server">
+                            <img src={backend} alt="server icon" />
+                            <div className="heading">SERVER</div>
+                            <div>NodeJS</div>
+                            <div>Express</div>
+                            <div>REST API</div>
+                            <div>Authentication/<br></br>Authorization</div>
+                            <div>Payment systems</div>
+                        </div>
+                        <div className="skills-card skills-card-database">
+                            <img src={database} alt="database icon" />
+                            <div className="heading">DATABASE</div>
+                            <div>mySQL</div>
+                            <div>MongoDB</div>
+                            <div>Password-</div>
+                            <div>Hashing</div>
+                            <div className="hidden-dash">-</div>
+                            <div className="hidden-dash">-</div>
+                        </div>
+                        <div className="skills-card skills-card-additional">
+                            <img src={additional} alt="additional icon" />
+                            <div className="heading">ADDITIONAL</div>
+                            <div>Unit Testing</div>
+                            <div>End to End-</div>
+                            <div>testing</div>
+                            <div>Continuous-</div>
+                            <div>Integration</div>
+                            <div className="hidden-dash">-</div>
+                        </div>
+                    </div>
+
+                    <div className="gallery span-row-2">
+                        <Gallery />
+                    </div>
+                    <footer className="footer span-row-2">
+                        <Link smooth to="/tablet/details/#portfolio">portfolio</Link>
+                        <Link smooth to="/tablet/details/#services" scroll={el => el.scrollIntoView({ behavior: "smooth", block: "start" })}>services</Link>
+                        <Link smooth to="/tablet/details/#hire">hire me</Link>
+                    </footer>
+                </TabletLayout>
+            </MediaQuery>
+
+
+            <MediaQuery minDeviceWidth={850}>
+                <LaptopLayout>
                     <Logo />
                     <Menu />
 
@@ -132,7 +213,7 @@ function Home() {
                         <Link smooth to="/details/#services" scroll={el => el.scrollIntoView({ behavior: "smooth", block: "start" })}>services</Link>
                         <Link smooth to="/details/#hire">hire me</Link>
                     </footer>
-                </TabletLayout>
+                </LaptopLayout>
             </MediaQuery>
         </>
     )
