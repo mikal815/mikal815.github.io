c0bfad9e571a08aaa4d83f593b7bdae5bf3caf7a
Mike M <mikal815@users.noreply.github.com>
2020-09-13 16:56:13 -0400

    Config

--- index.js
+++ index.js
@@ -53,15 +53,19 @@ app.post("/api/sendMail", (req, res) => {
 //     })
 // }
 
-if (process.env.NODE_ENV === "production") {
+// if (process.env.NODE_ENV === "production") {
 
-    app.use(express.static('client/build'));
+//     app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    });
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     });
 
-}
+// }
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html')
+})
 
 const port = process.env.PORT || 5000;
 
