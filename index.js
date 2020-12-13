fdecc846195328474a894a371f5520c27ea2fcbc
Mike M <mikal815@users.noreply.github.com>
2020-12-13 16:36:18 -0500

    updates

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const nodemailer = require("nodemailer");
-const sgTransport = require('nodemailer-sendgrid-transport');
 const path = require('path');
 require('dotenv').config();
-const sgMail = require('@sendgrid/mail')
-
+const mailgun = require('mailgun-js');
+const DOMAIN = 'mg.mmportfolio.dev';
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,40 +12,35 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 
+app.post('/api/sendMail', (req, res) => {
 
-app.post("https://api.sendgrid.com/v3/mail/send HTTP/1.1", (req, res) => {
+    const mg = mailgun({ apiKey: '3a3ed0b15f6b73c2eb5b88a0008ddd2b-4879ff27-29351e1d', domain: DOMAIN });
 
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-    const msg = {
-        to: process.env.MYEMAIL, // Change to your recipient
+    const data = {
+        to: 'mikal815@gmail.com', // Change to your recipient
         from: `${req.body.email}`, // Change to your verified sender
         subject: 'New message!/michaelmensinger.com',
         text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`,
         html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    }
-    sgMail
-        .send(msg)
-        .then(() => {
-            console.log('Email sent')
-        })
-        .catch((error) => {
-            console.error(error)
-        })
+    };
 
-
-})
-
-
-
-if (process.env.NODE_ENV === "production") {
-
-    app.use(express.static('client/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    mg.messages().send(data, function (error, body) {
+        console.log(body);
     });
 
-}
+});
+
+
+//if (apps.env.NODE_ENV === "production") {
+
+app.use(express.static('client/build'));
+
+//  app.get('*', (req, res) => {
+//    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//});
+
+//}
+
 
 const port = process.env.PORT || 5000;
 
@@ -55,42 +48,3 @@ app.listen(port, () => {
     console.log(`Server running at ${port}`)
 })
 
-
-
-
-
-
-
-/////////////////////////////
-
-// Instantiate the SMTP server
-// const smtpTrans = nodemailer.createTransport({
-//     host: 'smtp.gmail.com',
-//     port: 465,
-//     secure: true,
-//     auth: {
-//         user: process.env.MYEMAIL,
-//         pass: process.env.PASSWORD
-//     }
-// })
-
-// console.log(req.body.name, req.body.email, req.body.message)
-
-// Specify what the email will look like
-// const mailOpts = {
-//     from: `${req.body.email}`, // This is ignored by Gmail
-//     to: process.env.MYEMAIL,
-//     subject: 'New message!/michaelmensinger.com',
-//     text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
-// }
-
-// Attempt to send the email
-// smtpTrans.sendMail(mailOpts, (error, response) => {
-//     if (error) {
-//         res.send('contact-failure')
-//         console.log(error);
-//     }
-//     else {
-//         res.send('contact-success')
-//     }
-// })
