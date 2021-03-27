593393f4da3f20ca658fd29bc1284e0dbae864b5
Mike M <mikal815@users.noreply.github.com>
2021-03-27 19:56:43 -0400

    Test sending emails with Mailgun

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const path = require('path');
-const nodemailer = require('nodemailer');
+const API_KEY = 'MAILGUN_API_KEY';
+const DOMAIN = 'MAILGUN_DOMAIN';
+const mailgun = require('mailgun-js')({ apiKey: API_KEY, domain: DOMAIN });
+// const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 
@@ -14,37 +17,45 @@ app.use(cookieParser());
 
 app.post("/api/sendMail", (req, res) => {
 
-    // Instantiate the SMTP server
-    const smtpTrans = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            user: process.env.MYEMAIL,
-            pass: process.env.PASSWORD
-        }
-    })
+    const data = {
+        from: `${req.body.email}`,
+        to: 'mikal815@gmail.com',
+        subject: 'New message!/michaelmensinger.com',
+        text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+    };
+
+    mailgun.messages().send(data, (error, body) => {
+        console.log(body);
+    });
+
+
+    // const smtpTrans = nodemailer.createTransport({
+    //     host: 'smtp.gmail.com',
+    //     port: 465,
+    //     secure: true,
+    //     auth: {
+    //         user: process.env.MYEMAIL,
+    //         pass: process.env.PASSWORD
+    //     }
+    // })
 
     console.log(req.body.name, req.body.email, req.body.message)
 
-    // Specify what the email will look like
-    const mailOpts = {
-        from: `${req.body.email}`, // This is ignored by Gmail
-        to: process.env.MYEMAIL,
-        subject: 'New message!/michaelmensinger.com',
-        text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
-    }
+    // const mailOpts = {
+    //     from: `${req.body.email}`,
+    //     to: process.env.MYEMAIL,
+    //     subject: 'New message!/michaelmensinger.com',
+    //     text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+    // }
 
-    // Attempt to send the email
-    smtpTrans.sendMail(mailOpts, (error, response) => {
-        if (error) {
-            res.send('contact-failure') // Show a page indicating failure
-            console.log(error)
-        }
-        else {
-            res.send('contact-success') // Show a page indicating success
-        }
-    })
+    // smtpTrans.sendMail(mailOpts, (error, response) => {
+    //     if (error) {
+    //         res.send('contact-failure')
+    //     }
+    //     else {
+    //         res.send('contact-success')
+    //     }
+    // })
 
     // console.log(req.body)
 
