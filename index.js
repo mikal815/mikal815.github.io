4c0214bbc06f4736d33ecbdd5021bceff0f3c8f1
Mike M <mikal815@users.noreply.github.com>
2021-03-27 16:13:55 -0400

    config

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.post("/api/sendMail", (req, res) => {
 
 //if (apps.env.NODE_ENV === "production") {
 
-// app.use(express.static('client/build'));
+app.use(express.static('client/build'));
 
 // app.get('*', (req, res) => {
 //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
