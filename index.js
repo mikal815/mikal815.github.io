cb18a41a76be56b0f81671d257ef2bed59b8185a
Mike M <mikal815@users.noreply.github.com>
2021-04-02 21:25:01 -0400

    revert

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.get('*', (req, res) => {
 // }
 
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server running at ${port}`)
