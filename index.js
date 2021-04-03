28c8ad54ff59c8f43b73fd7c9a40d7483ae7141e
Mike M <mikal815@users.noreply.github.com>
2021-04-02 21:20:27 -0400

    update port

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.get('*', (req, res) => {
 // }
 
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
     console.log(`Server running at ${port}`)
