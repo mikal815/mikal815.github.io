dcb534a28075b52b1dcf5f7413aa787e6d7d4f36
Mike M <mikal815@users.noreply.github.com>
2021-04-05 21:28:15 -0400

    test

--- index.js
+++ index.js
@@ -73,9 +73,9 @@ app.post("/api/sendMail", (req, res) => {
 
 app.use(express.static('client/build'));
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'))
+});
 
 // }
 
